1fe72ba9bc28d572238c03129a095470fd4d0dda
lfp <linfengpu@qq.com>
2012-07-31 01:03:51 +0800

    add demo

---
+++ demo/src/main/java/com/lfp/Demo.java
@@ -0,0 +1,13 @@
+package com.lfp;
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("demo");
+	}
+
+}
