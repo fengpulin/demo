b7db8dcdd0a94190c38a213fcb548947abe191a7
lfp <linfengpu@qq.com>
2012-07-31 13:44:30 +0800

    建立空struts demo

---
+++ demo/src/main/java/com/lfp/struts/action/DemoAction.java
@@ -0,0 +1,10 @@
+package com.lfp.struts.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DemoAction extends ActionSupport{
+
+	public String show(){
+		return SUCCESS;
+	}
+}
